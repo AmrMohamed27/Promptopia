942bb485f1a158ee69d29c9151ee54ff743d7881
AmrMohamed27 <amrmohamedorigi44@gmail.com>
2024-09-03 23:57:06 +0300

    Prevent unauthed users from creating or editing

--- app/posts/create/page.js
+++ app/posts/create/page.js
@@ -1,24 +1,36 @@
+"use client";
 import CreateForm from "@components/CreateForm";
+import { useSession } from "next-auth/react";
 
-const page = () => {
+const Page = () => {
+  const { data: session } = useSession();
   return (
-    <section className="flex py-8 md:py-16 px-4 md:px-8 w-full items-start mb-32">
+    <section className="flex py-8 md:py-16 px-4 md:px-8 w-full items-start min-h-screen">
       <div className="w-full flex flex-col gap-16">
-        {/* Heading */}
-        <div className="flex flex-col items-start justify-center gap-8">
-          <h1 className="text-5xl md:text-6xl blue_gradient font-extrabold">
-            Create a Post
+        {session?.user === undefined ? (
+          <h1 className="text-3xl md:text-4xl font-bold text-blue-600">
+            Please Sign in to create a prompt
           </h1>
-          <p className="text-lg md:text-xl text-pale-blue md:max-w-[62%]">
-            Create and share amazing prompts with the world, and let your
-            imagination run wild.
-          </p>
-        </div>
-        {/* Form */}
-        <CreateForm />
+        ) : (
+          <>
+            {" "}
+            {/* Heading */}
+            <div className="flex flex-col items-start justify-center gap-8">
+              <h1 className="text-5xl md:text-6xl blue_gradient font-extrabold">
+                Create a Post
+              </h1>
+              <p className="text-lg md:text-xl text-pale-blue md:max-w-[62%]">
+                Create and share amazing prompts with the world, and let your
+                imagination run wild.
+              </p>
+            </div>
+            {/* Form */}
+            <CreateForm />
+          </>
+        )}
       </div>
     </section>
   );
 };
 
-export default page;
+export default Page;
--- app/posts/edit/[id]/page.js
+++ app/posts/edit/[id]/page.js
@@ -3,8 +3,10 @@
 import { useEffect, useState } from "react";
 import CreateForm from "@components/CreateForm";
 import Image from "next/image";
+import { useSession } from "next-auth/react";
 
 const Page = ({ params }) => {
+  const { data: session } = useSession();
   const id = params.id;
   const [post, setPost] = useState(null);
 
@@ -22,37 +24,43 @@ const Page = ({ params }) => {
     }
   }, [id]);
   return (
-    <section className="flex py-8 md:py-16 px-4 md:px-8 w-full items-start mb-32">
-      <div className="w-full flex flex-col gap-16">
-        {/* Heading */}
-        <div className="flex flex-col items-start justify-center gap-8">
-          <h1 className="text-5xl md:text-6xl blue_gradient font-extrabold">
-            Edit Post
-          </h1>
-          <p className="text-lg md:text-xl text-pale-blue md:max-w-[62%]">
-            Create and share amazing prompts with the world, and let your
-            imagination run wild.
-          </p>
-        </div>
-        {/* Form */}
-        {post ? (
-          <CreateForm
-            initialPrompt={post.prompt}
-            initialTagsString={post.tags.join(", ")}
-            isEdit
-            postId={id}
-          />
-        ) : (
-          <div className="fixed inset-0 flex items-center justify-center bg-white bg-opacity-75 backdrop-blur-sm z-50">
-            <Image
-              src="/assets/icons/LoadingIcon.svg"
-              alt="Loading..."
-              width={100}
-              height={100}
-            />
+    <section className="flex py-8 md:py-16 px-4 md:px-8 w-full items-start min-h-screen">
+      {session?.user === undefined ? (
+        <h1 className="text-3xl md:text-4xl font-bold text-blue-600">
+          Please Sign in to edit a prompt
+        </h1>
+      ) : (
+        <div className="w-full flex flex-col gap-16">
+          {/* Heading */}
+          <div className="flex flex-col items-start justify-center gap-8">
+            <h1 className="text-5xl md:text-6xl blue_gradient font-extrabold">
+              Edit Post
+            </h1>
+            <p className="text-lg md:text-xl text-pale-blue md:max-w-[62%]">
+              Create and share amazing prompts with the world, and let your
+              imagination run wild.
+            </p>
           </div>
-        )}
-      </div>
+          {/* Form */}
+          {post ? (
+            <CreateForm
+              initialPrompt={post.prompt}
+              initialTagsString={post.tags.join(", ")}
+              isEdit
+              postId={id}
+            />
+          ) : (
+            <div className="fixed inset-0 flex items-center justify-center bg-white bg-opacity-75 backdrop-blur-sm z-50">
+              <Image
+                src="/assets/icons/LoadingIcon.svg"
+                alt="Loading..."
+                width={100}
+                height={100}
+              />
+            </div>
+          )}
+        </div>
+      )}
     </section>
   );
 };
