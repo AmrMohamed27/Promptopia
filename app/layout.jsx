c08dac274a2e36e3a67a5e9b962a135f3f2501e2
AmrMohamed27 <amrmohamedorigi44@gmail.com>
2024-09-21 16:50:13 +0300

    Use next/font to optimize fonts loading

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,15 +3,21 @@ import Navbar from "@components/navbar/Navbar";
 import Footer from "@components/common/Footer";
 import Background from "@components/common/Background";
 import { Providers } from "./providers";
+import { Inter } from "next/font/google";
 
 export const metadata = {
   title: "Promptopia",
   description: "Discover and Share AI Prompts",
 };
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className={inter.variable}>
       <body className="relative dark:bg-black dark:text-white">
         <Providers>
           <div className="block dark:hidden">
